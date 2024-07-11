543c26676e2f2b2441d809ab175160e386983d1d
stancobridge <stancoproof@gmail.com>
2024-07-11 02:05:23 +0100

    fix: set code sniffer to use psr 12 standard

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,8 +6,5 @@ $finder = (new PhpCsFixer\Finder())
 
 return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
-    ->setRules([
-        '@Symfony' => true,
-        '@PSR12' => true,
-    ])
+    ->setRules(['@PSR12' => true])
     ->setFinder($finder);
